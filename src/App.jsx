2bd40441afc16046b74d2ec673ba19f4c0f7a36e
fazildev12 <fazilfazillisaf17@gmail.com>
2024-07-22 00:28:31 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom/client";
 
 import Counter from "remote/Counter";
 
-
-
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
+  <div>
     <div>React Parent App</div>
-    <button type="button" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Purple</button>
+    <p>The bellow content is from other app</p>
     <Counter />
   </div>
 );
