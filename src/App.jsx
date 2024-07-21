25e29d489e2fa8feba155ef6a57714ca2aef21b1
fazildev12 <fazilfazillisaf17@gmail.com>
2024-07-21 23:49:09 +0530

    Removed and added a button

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,8 @@ import "./index.scss";
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: react-parent-app</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
+    <div>React Parent App</div>
+    <button type="button" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Purple</button>
   </div>
 );
 const rootElement = document.getElementById("app")
